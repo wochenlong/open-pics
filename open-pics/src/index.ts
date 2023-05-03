59efc37a4a1d97380bf32831652b246d8f4b9ab1
windsing <117965575+wochenlong@users.noreply.github.com>
2023-05-03 15:35:02 +0800

    Update index.ts
    不可用，但是备份

--- open-pics/src/index.ts
+++ open-pics/src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema, Session, h } from "koishi";
+import { Context, Session, Schema, segment, h } from "koishi";
 import { readdir } from "node:fs/promises";
 import { resolve } from "path";
 import { pathToFileURL } from "url";
@@ -14,16 +14,25 @@ export const usage = `
 
 在imagePath处填入你的图片文件夹绝对路径即可
 
+
+[意见反馈](https://forum.koishi.xyz/t/topic/1727)
+
 `;
 
 export interface Config {
   imagePath: string;
+  output: string;
 }
 
 export const Config: Schema<Config> = Schema.object({
   imagePath: Schema.string().description("图片文件夹路径").default("D:/img/xp"),
-});
-
+  output: Schema.union([
+    Schema.const("figure").description("以聊天记录形式发送"),
+    Schema.const("image").description("以图片形式发送"),
+  ])
+    .description("输出方式。")
+    .default("image"),
+}).description("进阶设置");
 export const using = [];
 
 // 导出一个异步函数，接收两个参数：Context 和 Config
@@ -32,14 +41,7 @@ export async function apply(ctx: Context, config: Config) {
   const logger = ctx.logger("open-pics");
 
   // 定义一个返回指定范围内随机整数的函数
-  function mathRandomInt(a: number, b: number): number {
-    // 如果 a 大于 b，则交换 a 和 b 的值
-    if (a > b) {
-      [a, b] = [b, a]; // 使用解构赋值交换 a 和 b 的值
-    }
-    // 返回 a 到 b 之间的随机整数
-    return Math.floor(Math.random() * (b - a + 1) + a);
-  }
+
   async function getRandomFileIn(path) {
     const files = await readdir(path);
     return join(path, Random.pick(files));
@@ -48,26 +50,36 @@ export async function apply(ctx: Context, config: Config) {
   // 定义一个命令处理函数
   async function handleCommand(session: Session) {
     try {
-      // 获取图片文件夹中的所有文件名
-      const files = fs.readdirSync(config.imagePath);
-      // 随机选择一个文件名
-      const fileName = files[mathRandomInt(0, files.length - 1)];
-
-      // 发送本地图片消息
-
-      await session.send(
-        h.image(
+      if (config.output === "figure") {
+        // 如果 `output_type` 的值为 "figure"，则将图片以聊天记录形式发送
+        const result = segment("figure");
+        result.children.push(
+          segment("message", {
+            userId: session.selfId,
+            nickname: "system",
+          })
+        );
+        result.children.push(
+          segment("image", {
+            src: pathToFileURL(
+              resolve(__dirname, await getRandomFileIn(config.imagePath))
+            ).href,
+          })
+        );
+        return result;
+      } else {
+        // 如果 `output_type` 的值不是 "figure"，则以图片形式发送
+        return h.image(
           pathToFileURL(
             resolve(__dirname, await getRandomFileIn(config.imagePath))
           ).href
-        )
-      );
+        );
+      }
     } catch (error) {
       logger.error(error.message);
       await session.send("获取图片失败，请稍后再试");
     }
   }
-
   // 定义一个命令
   ctx
     .command("xp", "获取一张随机的 xp 图片")
